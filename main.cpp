8caef191aa4fa18e3f6d906cf6d4f974ec381147
saber king <saberking321@gmail.com>
2020-05-02 00:10:25 +0100

    1111

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
+
+
 #include "includes.hpp"
 
+sf::Font font;
+
+ sf::RenderWindow window(sf::VideoMode(1200, 700), "Gob");
+
+
+
+
+
+
+void setup(){
+ 
+    srand(time(NULL));
+        font.loadFromFile("fonts/thestrong.ttf");
+  
+}
+
 int main(){
-    getchar();
+    setup();
+
+
+    sf::Clock clock;
+    sf::Time elapsed;
+    sf::Event event;
+
+    window.setFramerateLimit(20);
+    while (window.isOpen())
+    {
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if(event.type==sf::Event::MouseButtonPressed){
+                sf::Vector2i pos=sf::Mouse::getPosition(window);
+
+            }
+        }
+        elapsed = clock.getElapsedTime();
+
+    }
+
     return 0;
 }
