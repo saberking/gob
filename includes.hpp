8680d1ed0af1fd5584f1fb5df5f58b22a9c13c7a
Saber <saberking321@protonmail.com>
2022-11-05 23:00:05 +0000

    linux-compatibility

--- includes.hpp
+++ includes.hpp
@@ -4,5 +4,16 @@
 #include<string>
 #include <deque>
 #include <SFML/Graphics.hpp>
+
+
+#ifdef _WIN32
 #include <conio.h>
+#elif defined __unix__
+#include <curses.h>
+#elif defined __APPLE__
+// do something for mac
+#endif
+
+
+
 #include <stdio.h>
